aae8cd4d330179c16d68faaac67707d256e14cfe
smadshark <smadshark@gmail.com>
2021-01-27 01:45:17 +0900

    eatgo init

--- eatgo/eatgo-api/src/main/java/com/sonbro/eatgo/repository/MenuItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sonbro.eatgo.repository;
-
-import com.sonbro.eatgo.domain.MenuItem;
-import java.util.List;
-
-public interface MenuItemRepository {
-    List<MenuItem> findAllByRestaurantId(Long restaurantId);
-}
